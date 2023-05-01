2e4716f776fba91a576ede7c655f0c33cd4ab26c
Felipe Carvalho <felipe_pc7@hotmail.com>
2023-05-01 15:10:36 -0300

    create basic upload logic

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -3,7 +3,7 @@ import * as C from "./Title.styles";
 export const Title = () => {
     return (
         <C.TitleSection>
-            <h1>Suas imagens</h1>
+            <h1>Sua Galeria</h1>
             <p>Salve suas imagens favoritas!</p>
         </C.TitleSection>
     );
--- src/components/UploadForm/UploadForm.jsx
+++ src/components/UploadForm/UploadForm.jsx
@@ -1,13 +1,36 @@
-import * as C from "./UploadForm.styles";
+import { useEffect, useState } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 
+// styles import
+import * as C from "./UploadForm.styles";
+
 export const UploadForm = () => {
+    const [file, setFile] = useState(null);
+    const allowedTypes = ["image/jpeg", "image/png"];
+
+    // useEffect just to log the file value, remove this later
+    useEffect(() => {
+        console.log(file);
+    }, [file]);
+
+    const changeFile = async e => {
+        let selectedFile = e.target.files[0];
+
+        // Check if file is available and if his type is allowed
+        if (selectedFile && allowedTypes.includes(selectedFile.type)) {
+            setFile(selectedFile);
+        } else {
+            setFile(null);
+            console.log("Erro ao enviar a imagem");
+        }
+    };
+
     return (
         <C.Form>
             <label htmlFor="file">
                 <AiOutlinePlus size={32} />
             </label>
-            <input type="file" name="file" id="file" />
+            <input onChange={changeFile} type="file" name="file" id="file" />
         </C.Form>
     );
 };
